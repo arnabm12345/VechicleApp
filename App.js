d5a8d95aaebe0ea9b9c4d7ba1df7ec0eeff93537
Arnab <arnabm12345@gmail.com>
2023-03-18 13:26:46 +0530

    Welcome Screen

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { StyleSheet, View, Image, Text,Button,StatusBar } from 'react-native';
+import Colors from './constants/color';
+import PrimaryButton from './Components/ui/PrimaryButton';
+import Title from './Components/ui/Title';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Papai!!</Text>
+      <View style={styles.container}>
+         <Text style={styles.title}>Vechicle Breakdown App</Text> 
+       <View style={styles.image}>
+      <Image source={require('./assets/220-2203211_home-car-breakdown-logo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.button}>
+      <PrimaryButton>Let`s Go</PrimaryButton>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+     alignItems: 'center',
+    backgroundColor: Colors.accent500,
   },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 50,
+    paddingTop:10,
+    borderRadius:70,
+    justifyContent:'center',
+    backfaceVisibility:'hidden',
+  },
+  title: {
+    fontSize: 22,
+    borderBottomWidth :3,
+    borderBottomColor: '#000',
+    fontWeight: 'bold',
+    color: Colors.primary500,
+    marginTop:70,
+  },
+  image:{
+    marginTop:150,
+  },
+  button:{
+    paddingTop:50,
+  }
 });
---
+++ Components/ui/PrimaryButton.js
@@ -0,0 +1,45 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+import Colors from '../../constants/color';
+
+function PrimaryButton({ children, onPress,style }) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary600 }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 14,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize:20,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
