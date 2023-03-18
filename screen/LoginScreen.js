981b95a3f371426c492d4936b461f480db290fa0
Arnab <arnabm12345@gmail.com>
2023-03-18 21:48:02 +0530

    storing-managing-the-user-auth-state version 2

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -1,15 +1,20 @@
 import AuthContent from '../Components/Auth/AuthContent';
-import { useState } from 'react';
+import { useState ,useContext} from 'react';
 import LoadingOverlay from '../Components/ui/LoadingOverlay';
 import { login } from '../util/auth';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
+
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx=useContext(AuthContext);
 
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try{
-    await login(email, password);
+    const token=await login(email, password);
+     authCtx.authenticate(token);
     }
     catch (error){
      Alert.alert('Authenticaion Failed!','Could not logged in! Please Check your creditional .Or try again Later');
--- screen/SignupScreen.js
+++ screen/SignupScreen.js
@@ -1,13 +1,23 @@
 import AuthContent from '../Components/Auth/AuthContent';
 import { createUser } from '../util/auth';
-import { useState } from 'react';
+import { useState,useContext } from 'react';
 import LoadingOverlay from '../Components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
+import { Alert } from 'react-native';
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+const authCtx=useContext(AuthContext);
+
  async function signUpHandler({email,password}){
   setIsAuthenticating(true);
-  await createUser(email, password);
+  try{
+ const token= await createUser(email, password);
+  authCtx.authenticate(token);
+  }
+  catch(error){
+    Alert.alert('Authentication failed','could not create New user . Please try again later');
+  }
   setIsAuthenticating(false);
   }
   if (isAuthenticating) {
