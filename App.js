91b50155ccdc4ee78d86982d3c95a986da676f79
Arnab <arnabm12345@gmail.com>
2023-03-18 15:40:35 +0530

    Initialization of Login Screen

--- App.js
+++ App.js
@@ -1,51 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+//import * as React from 'react';
 import { StyleSheet, View, Image, Text,Button,StatusBar } from 'react-native';
 import Colors from './constants/color';
 import PrimaryButton from './Components/ui/PrimaryButton';
 import Title from './Components/ui/Title';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screen/LoginScreen';
+import WelcomeScreen from './screen/WelcomeScreen';
+
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const [ClickState,setClickState] =useState(false);
+  function clickHandeler(){
+      setClickState(true);
+  }
+  if(ClickState==true){
+      return(
+      <LoginScreen/>
+      );
+  }
   return (
-      <View style={styles.container}>
-         <Text style={styles.title}>Vechicle Breakdown App</Text> 
-       <View style={styles.image}>
-      <Image source={require('./assets/220-2203211_home-car-breakdown-logo.png')} style={styles.logo} />
-      </View>
-      <View style={styles.button}>
-      <PrimaryButton>Let`s Go</PrimaryButton>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <>
+     <StatusBar style="auto" />
+      <WelcomeScreen props={clickHandeler}/>
+    </>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //justifyContent: 'center',
-     alignItems: 'center',
-    backgroundColor: Colors.accent500,
-  },
-  logo: {
-    width: 150,
-    height: 150,
-    marginBottom: 50,
-    paddingTop:10,
-    borderRadius:70,
-    justifyContent:'center',
-    backfaceVisibility:'hidden',
-  },
-  title: {
-    fontSize: 22,
-    borderBottomWidth :3,
-    borderBottomColor: '#000',
-    fontWeight: 'bold',
-    color: Colors.primary500,
-    marginTop:70,
-  },
-  image:{
-    marginTop:150,
-  },
-  button:{
-    paddingTop:50,
-  }
-});
---
+++ screen/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text,View} from 'react-native';
+
+function LoginScreen(){
+
+    return(
+     <View>
+        <Text>This is a Login Screen</Text>
+     </View>
+    );
+}
+export default LoginScreen;
